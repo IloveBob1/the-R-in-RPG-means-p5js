8914f0114a098eff77858f86442b1d934ebd3155
Kirbyx09 <101605075+IloveBob1@users.noreply.github.com>
2025-04-03 20:35:04 -0400

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,26 @@
+const SCL = 10
+
+var player;
+var back;
+var gameOver = false;
+
+function setup() {
+  createCanvas(400, 400);
+  player = new Player(int(width/(2*SCL)), int(height/(2*SCL)));
+  back = new Background();
+  back.setData();
+}
+
+function draw() {
+  if (!gameOver) {
+    back.draw();
+    noStroke();
+    player.draw();
+    player.update()
+  } else {
+    fill(255)
+    background(0)
+    textSize(50)
+    text("Game Over", width/2-130, height/2)
+  }
+}
